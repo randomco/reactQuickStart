5d4769ee1bb16d96e5ec6d4135afe0d7bd62543f
Prabhakar Desai <111271@cognizant.com>
2020-10-08 13:02:58 +0530

    base version

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import Car from './Car';
+//import Car from './Car';
+import RecipeReviewCard from './RecipeReviewCard';
 
 function App() {
     return (
@@ -9,17 +10,19 @@ function App() {
             <header className="App-header">
                 <img src="https://portalfast.ite.mypepsico.com/ep/common/portal/unprotected/dev/pwa/logo.svg" className="App-logo" alt="logo" />
                 <p>
-                    Hello, I am running on <code>AWS Cloud!</code>
+                    Hello, I am a &nbsp;
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        React
+                    </a>
+                    &nbsp; Widget running on <br/>
+                    <code>~ AWS Cloud! ~</code>
                 </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-        </a>
-                <Car />
+                <RecipeReviewCard/>
             </header>
         </div>
     );
